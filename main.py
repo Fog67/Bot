f2f7f65be9ffdc7df386bb1cdf0a4897591aaa19
Iurii Pereshein <yapereshein@edu.hse.ru>
2026-04-22 11:13:02 +0500

    m

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from mock_reader import mock_reader
-from normalizer import DeviceSchema
+from normalizer import Device
 import asyncio
 import json
 from datetime import datetime
@@ -8,7 +8,7 @@ from datetime import datetime
 if __name__ == "__main__":
     async def test():
         async for packet in mock_reader():
-            print(DeviceSchema(**packet))
+            print(Device(**packet))
 
 
     asyncio.run(test())
